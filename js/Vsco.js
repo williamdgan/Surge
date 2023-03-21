c56085194e40c311dffcfd0f6a1ca567ed2174ab
Mubdao <46107656+Mubdao@users.noreply.github.com>
2023-03-21 14:53:59 +0800

    Update Vsco.js

--- js/Vsco.js
+++ js/Vsco.js
@@ -5,11 +5,11 @@ VSCO 解锁高级特权
 hostname = vsco.co, api.revenuecat.com
 
 QuantumultX:
-^https:\/\/(api\.revenuecat\.com\/v\d\/subscribers|vsco\.co\/api\/subscriptions\/\d\.\d\/user-subscriptions)\/ url script-response-body https://raw.githubusercontent.com/Mubdao/Example/main/vsco.js
+^https:\/\/(api\.revenuecat\.com\/v\d\/subscribers|vsco\.co\/api\/subscriptions\/\d\.\d\/user-subscriptions)\/ url script-response-body https://raw.githubusercontent.com/Mubdao/Surge/main/js/Vsco.js
 
 Surge:
 [Script]
-VSCO = type=http-response,pattern=^https:\/\/(api\.revenuecat\.com\/v\d\/subscribers|vsco\.co\/api\/subscriptions\/\d\.\d\/user-subscriptions)\/,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Mubdao/Example/main/vsco.js,script-update-interval=0
+VSCO = type=http-response,pattern=^https:\/\/(api\.revenuecat\.com\/v\d\/subscribers|vsco\.co\/api\/subscriptions\/\d\.\d\/user-subscriptions)\/,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Mubdao/Surge/main/js/Vsco.js,script-update-interval=0
 */
 
 
