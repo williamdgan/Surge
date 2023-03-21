0f082c11313c061b481696a291e59b1a3692be08
Mubdao <46107656+Mubdao@users.noreply.github.com>
2023-03-21 14:39:06 +0800

    Update Lightroom.js

--- js/Lightroom.js
+++ js/Lightroom.js
@@ -1,10 +1,16 @@
 /*
-QX：
+解锁lightroom Premium
+
+[MITM]
+mitm= photos.adobe.io
+
+QuantumultX:
 ^https:\/\/photos\.adobe\.io\/v2\/accounts* url script-response-body https://raw.githubusercontent.com/Mubdao/Example/main/lightroom.js
-SG：
+
+Surge:
 Lightroom = type=http-response,pattern=^https:\/\/photos\.adobe\.io\/v2\/accounts,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Mubdao/Example/main/lightroom.js,script-update-interval=-1
-mitm:photos.adobe.io
 */
+
 
 body = $response.body.replace(/while.{7}\n/, "");
 let obj = JSON.parse(body);
