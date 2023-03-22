7a0ac8cd332413e2740d6792d0a06d124a63b1aa
Mubdao <46107656+Mubdao@users.noreply.github.com>
2023-03-22 18:41:38 +0800

    Delete hav.is

--- js/jav.js
+++
@@ -1,8 +0,0 @@
-let obj = JSON.parse(body);
-      if (obj.data.splash_ad) {
-        obj.data.splash_ad.enabled = false;
-        obj.data.splash_ad.overtime = 0;
-      }
-      body = JSON.stringify(obj);
-    } catch (error) {
-      console.log(`JavDB, 出现异常: ` + error);
