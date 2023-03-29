23302064e483b796cc0cc3881ea314b94890629b
Mubdao <46107656+Mubdao@users.noreply.github.com>
2023-03-29 09:22:38 +0800

    更新 NetISP.js

--- js/NetISP.js
+++ js/NetISP.js
@@ -14,7 +14,7 @@ let uniqueLocationsArray = [...new Set(locationsArray)];
 let uniqueLocations = uniqueLocationsArray.join("  ");
 
   body = {
-    title: "𝗡𝗲𝘁 𝗜𝗦𝗣",
+    title: "ɴᴇᴛ ɪsᴘ",
 		//${emoji}
     content: `${ip}  ${isp} \n${uniqueLocations}`
    // icon: "key.icloud", 'icon-color': "#FF5A9AF9"
