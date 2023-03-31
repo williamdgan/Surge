30d7fb87599398eabaccd9fd877b9b74347dc616
Mubdao <46107656+Mubdao@users.noreply.github.com>
2023-03-31 12:29:06 +0800

    更新 NetISP.js

--- js/NetISP.js
+++ js/NetISP.js
@@ -1,4 +1,4 @@
-let url = "http://ip-api.com/json/?lang=en"
+let url = "http://ip-api.com/json/?lang=zh-cn"
 $httpClient.get(url, function(error, response, data){
 let jsonData = JSON.parse(data)
 let ip = jsonData.query
@@ -14,7 +14,7 @@ let uniqueLocationsArray = [...new Set(locationsArray)];
 let uniqueLocations = uniqueLocationsArray.join("  ");
 
   body = {
-    title: "ɴᴇᴛ ɪsᴘ",
+    title: "Net ISP",
 		//${emoji}
     content: `${ip}  ${isp} \n${uniqueLocations}`
    // icon: "key.icloud", 'icon-color': "#FF5A9AF9"
