a08dd2f3e78d0abdd3c93f524d9dc6b02fd394d6
Mubdao <46107656+Mubdao@users.noreply.github.com>
2023-03-31 12:33:05 +0800

    更新 NetISP.js

--- js/NetISP.js
+++ js/NetISP.js
@@ -1,4 +1,4 @@
-let url = "http://ip-api.com/json/?lang=zh-cn"
+let url = "http://ip-api.com/json/?lang=zh-CN"
 $httpClient.get(url, function(error, response, data){
 let jsonData = JSON.parse(data)
 let ip = jsonData.query
