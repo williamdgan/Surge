3a3099f3852d474512da2e541d6d62ab68516120
Mubdao <46107656+Mubdao@users.noreply.github.com>
2023-04-17 22:51:45 +0800

    Delete  Baidusearchresults-Ads.js

--- js/Baidusearchresults-Ads.js
+++
@@ -1,5 +0,0 @@
-let rHead = "<head>";
-let newStyle =
-  "<head><style> .wpoScript, .ec_ad_results, .ec_wise_ad, .page-copyright{display:none!important} </style>";
-let body = $response.body.replace(rHead, newStyle);
-$done({ body });
